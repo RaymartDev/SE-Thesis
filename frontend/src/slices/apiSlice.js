a2d13df182ce15c17e28b0640007b2282a38e2f4
RaymartDev <raymart.sarmiento23@outlook.com>
2023-11-30 07:01:59 +0800

    Final

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://setech.onrender.com' })
+const baseQuery = fetchBaseQuery({ baseUrl: '' })
 
 export const apiSlice = createApi({
     baseQuery,
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://setech.onrender.com',
+        target: 'http://localhost:5000',
         changeOrigin: true
       }
     }
