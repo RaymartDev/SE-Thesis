f68b2c92cf8a671679c5d8c389fb47732941f198
RaymartDev <raymart.sarmiento23@outlook.com>
2023-11-29 14:32:38 +0800

    Added database access and working routes

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -5,7 +5,7 @@ const notFound = (req, res, next) => {
 }
 
 const errorHandler = (err, req, res, next) => {
-    let statusCode = res.statasCode === 200 ? 500 : res.statusCode
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode
     let message = err.message
 
     if(err.name === 'CastError' || err.kind === 'ObjectId') {
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,11 +3,16 @@ const router = express.Router()
 import {
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    getUserProfile
 } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.post('/logout', logoutUser)
+router
+    .route('/profile')
+    .get(protect, getUserProfile)
 
 export default router
