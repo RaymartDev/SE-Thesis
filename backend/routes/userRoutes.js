587e5d34dee1e4d0f3fa327a0f76143e839325bc
RaymartDev <raymart.sarmiento23@outlook.com>
2023-12-06 23:22:29 +0800

    Added all relevant routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,15 +4,29 @@ import {
     registerUser,
     loginUser,
     logoutUser,
-    getUserProfile
+    getUserProfile,
+    updateProfile,
+    getJob,
+    createJob,
+    saveJob,
+    searchJob,
 } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.post('/logout', logoutUser)
+
+// profile related routes
 router
     .route('/profile')
     .get(protect, getUserProfile)
+    .put(protect, updateProfile)
+
+// job related routes
+router.get('/jobs/:page', protect,getJob)
+router.get('/jobs', protect, searchJob)
+router.post('/jobs/create', protect , createJob)
+router.post('/jobs/save', protect, saveJob)
 
 export default router
